17f172543d2d774a8e80816968c5797c30ff7fc6
TwoFacedNecromancer <61152616+TwoFacedNecromancer@users.noreply.github.com>
2024-09-26 10:55:45 -0500

    can now jump while sliding

--- scripts/slidingState.gd
+++ scripts/slidingState.gd
@@ -3,6 +3,8 @@ extends State
 class_name SlidingState
 
 @export var ground_state : State
+@export var air_state : State
+@export var jump_velocity : float = -300.0
 
 
 func state_process(delta):
@@ -10,3 +12,10 @@ func state_process(delta):
 	if(character.is_on_floor() and !Input.is_action_pressed("slide")):
 		playback.travel("move")
 		next_state = ground_state
+	if(character.is_on_floor() and Input.is_action_pressed("jump")):
+		jump()
+
+func jump():
+	character.velocity.y = jump_velocity
+	next_state = air_state
+	playback.travel("jump_start")
