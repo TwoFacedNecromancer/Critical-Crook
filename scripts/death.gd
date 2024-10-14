b1e43d53634e4046948b892dc95993d7815a8b76
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2024-10-13 22:14:21 -0500

    Added a really basic main menu
    It is not very good, need to get a sprite to replace the placeholder.

--- scripts/death.gd
+++ scripts/death.gd
@@ -2,13 +2,5 @@ extends State
 
 class_name DeathState
 
-@onready var level = get_parent().get_parent().get_parent()
-
-func reload():
-	level.get_tree().reload_current_scene()
-
-func _ready() -> void:
-	level.connect("sendToPlayer", Callable(self,"reload"))
-
-func state_process(delta):
-	pass
+#this currently does nothing because apparently I didn't even need it.
+#*sigh*
--- scripts/killbox.gd
+++ scripts/killbox.gd
@@ -6,7 +6,8 @@ signal death_signal
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print("then perish...")
 	killbox_timer.start()
 
 func _on_killbox_timer_timeout() -> void:
-	emit_signal("death_signal")
+	get_tree().reload_current_scene()
