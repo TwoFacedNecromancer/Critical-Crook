6201416fca6aba5cd39cf271954531111a6cf34a
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2024-10-19 15:57:31 -0500

    Added death, skull bat, at some other fixes

---
+++ scripts/skull_bat.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var movementType: bool = false
+
+@onready var rayCastRight = $RayCastRight
+@onready var rayCastLeft = $RayCastLeft
+@onready var sprite = $"Skull Bat Sprite"
+
+const speed = 60
+var direction = -1
+var timer = 360
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if movementType == true:
+		position.x += speed * direction * delta
+		if rayCastRight.is_colliding():
+			direction = -1
+			sprite.flip_h=false
+		elif rayCastLeft.is_colliding():
+			direction = 1
+			sprite.flip_h=true
