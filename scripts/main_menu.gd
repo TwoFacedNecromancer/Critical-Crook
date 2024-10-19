5def7fa20005a704bdcb326df370dc3caf4bff92
TwoFacedNecromancer <61152616+TwoFacedNecromancer@users.noreply.github.com>
2024-10-19 16:05:13 -0500

    level testing

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
 
 
 func _on_options_button_pressed() -> void:
--- scripts/scene_zone.gd
+++ scripts/scene_zone.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@export var nextScene: String
+@export var nextScene : String
 
-func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file(nextScene)
+func _on_body_entered(body: CharacterBody2D) -> void:
+	if(body.name == "player"):
+		get_tree().change_scene_to_file(nextScene)
