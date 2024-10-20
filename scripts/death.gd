00238183dd55ca9524f0f48d9fe8c4f6518a903a
TwoFacedNecromancer <61152616+TwoFacedNecromancer@users.noreply.github.com>
2024-10-20 14:06:44 -0500

    nothing

--- scripts/death.gd
+++ scripts/death.gd
@@ -2,7 +2,10 @@ extends State
 
 class_name DeathState
 
+var deathtoggle : bool = false
 
 func _process(delta: float) -> void:
 	if get_parent().get_parent().isdead==true:
-		playback.travel("death")
+		if(deathtoggle == false):
+			playback.travel("death")
+			deathtoggle = true
